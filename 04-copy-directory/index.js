9fbdde82a1d767ebdb2bccf9f1ced21daaf795c4
Alexandr Lazar <lazarforcode@gmail.com>
2021-11-11 20:26:41 +0300

    fix: fix so,e problems with 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,28 +2,29 @@ const fs = require('fs');
 const path = require('path');
 let folder = path.join(__dirname, 'files');
 let folder2 = folder+'-copy';
-fs.readdir(__dirname, {withFileTypes: true}, (error, files) => {
+fs.readdir(folder, {withFileTypes: true}, (error, files) => {
   if(error){
     throw error
   }else{
-    files.forEach(file => {
-      if(file.isDirectory()){
-        fs.stat(folder2, err => {
-          if(!err){
-            fs.rmdir(folder2, {recursive:true}, err => {if(err) throw err});
-          }else if(err.code === 'ENOENT') {
-            console.log('директории нет');
-          }})
-        fs.mkdir(folder2, {recursive: true}, error => {if(error) throw error});
-        fs.readdir(folder, {withFileTypes: true}, (error, folderFiles) => {
-          if(error) throw error;
-          folderFiles.forEach(fileFold =>{
-            console.log(fileFold.name);
-            fs.copyFile(folder+`/${fileFold.name}`, folder2+`/${fileFold.name}`, (err) => {if(err) throw err});
-          })
-        })
-        
+    fs.access(folder2, (error) => {
+      if (!error) {
+        fs.readdir(folder2, (error, files) => {
+          if (error) throw error;
+          for (let file of files) {
+            fs.unlink(path.join(folder2, file), (error) => {
+              if (error) throw error;
+              cop();
+            });
+          }
+        });
       }
-    })
+    });
+    function cop(){
+      fs.mkdir(folder2, {recursive: true}, error => {if(error) throw error});
+      files.forEach(fileFold =>{
+        fs.copyFile(folder+`/${fileFold.name}`, folder2+`/${fileFold.name}`, (err) => {if(err) throw err});
+      })
+    }
+    cop();
   }
 })
